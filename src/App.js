891863ebc370876f67aec38302420ba35677cfb2
ERR DEMON <errordeweloper@gmail.com>
2023-10-17 21:54:39 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+
+function App() {
+  const {onToggleButton, tg} = useTelegram()
+
+  useEffect(() => {
+    tg.ready()
+  }, []);
+
+
+
+  return (
+    <div className="App">
+      <button onClick={onToggleButton}>toggle</button>
+    </div>
+  );
+}
+
+export default App;
